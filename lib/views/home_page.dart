a70b8e5ac24fb7f24cb86eca6d1316c9b69681a4
Trần Ngọc Quý <quytn@etc.vn>
2022-12-27 16:56:51 +0700

    home update

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -12,151 +12,223 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(80),
+        preferredSize: Size.fromHeight(150),
         child: AppBar(
-          toolbarHeight: 100,
-          title: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Trần Ngọc Quý',
-                style: TextStyle(fontSize: 16),
-              ),
-              Text(
-                'Email: quy@gmail.com',
-                style: TextStyle(fontSize: 14),
-              )
-            ],
-          ),
-          leading: Container(
-            padding: EdgeInsets.only(left: 10),
-            child: CircleAvatar(
-              child: CircleAvatar(
-                  radius: 25,
-                  backgroundImage: AssetImage('assets/images/user_icon.jpg')),
-            ),
-          ),
-          backgroundColor: Colors.purple,
-        ),
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+          toolbarHeight: 150,
+          title: Container(
+            margin: EdgeInsets.only(left: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  'Tiện ích',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  'Trần Ngọc Quý',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
                 ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  'Development',
+                  style: TextStyle(fontSize: 14, color: Colors.black),
+                )
               ],
             ),
-            margin: EdgeInsets.only(top: 10, left: 10, bottom: 10),
           ),
-          Container(
-            child: GridView.count(
-              crossAxisCount: 4,
+          leadingWidth: 90,
+          leading: Container(
+            margin: EdgeInsets.only(left: 10),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                  image: AssetImage('assets/images/avatar.png'),
+                  fit: BoxFit.contain),
+            ),
+          ),
+          actions: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).pushNamed('/Task');
-                  },
-                  child: Container(
+                Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.deepPurple.shade900),
+                    margin: EdgeInsets.only(right: 10, bottom: 40),
+                    width: 40,
+                    height: 40,
+                    child: Icon(
+                      Icons.notifications,
+                    )),
+              ],
+            )
+          ],
+          backgroundColor: Colors.white,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Project',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              margin: EdgeInsets.only(top: 20, left: 10, bottom: 10),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 10, right: 10),
+              height: 150,
+              child: ListView(
+                // This next line does the trick.
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Container(
+                    width: 160.0,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    width: 160.0,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    width: 160.0,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    width: 160.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 160.0,
+                    color: Colors.orange,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Tiện ích',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              margin: EdgeInsets.only(top: 10, left: 10, bottom: 10),
+            ),
+            Container(
+              child: GridView.count(
+                crossAxisCount: 4,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pushNamed('/Task');
+                    },
+                    child: Container(
+                      color: Colors.green,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(
+                            Icons.home,
+                            color: Colors.white,
+                          ),
+                          Text("Home", style: TextStyle(color: Colors.white))
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
                     color: Colors.green,
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Icon(
-                          Icons.home,
+                          Icons.account_circle,
                           color: Colors.white,
                         ),
-                        Text("Home", style: TextStyle(color: Colors.white))
+                        Text("Account", style: TextStyle(color: Colors.white))
                       ],
                     ),
                   ),
-                ),
-                Container(
-                  color: Colors.green,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Icon(
-                        Icons.account_circle,
-                        color: Colors.white,
-                      ),
-                      Text("Account", style: TextStyle(color: Colors.white))
-                    ],
+                  Container(
+                    color: Colors.green,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(
+                          Icons.message,
+                          color: Colors.white,
+                        ),
+                        Text("Messages", style: TextStyle(color: Colors.white))
+                      ],
+                    ),
                   ),
-                ),
-                Container(
-                  color: Colors.green,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Icon(
-                        Icons.message,
-                        color: Colors.white,
-                      ),
-                      Text("Messages", style: TextStyle(color: Colors.white))
-                    ],
+                  Container(
+                    color: Colors.green,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(
+                          Icons.list_alt,
+                          color: Colors.white,
+                        ),
+                        Text("Orders", style: TextStyle(color: Colors.white))
+                      ],
+                    ),
                   ),
-                ),
-                Container(
-                  color: Colors.green,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Icon(
-                        Icons.list_alt,
-                        color: Colors.white,
-                      ),
-                      Text("Orders", style: TextStyle(color: Colors.white))
-                    ],
-                  ),
-                ),
-              ],
-              padding: EdgeInsets.all(10),
-              shrinkWrap: true,
-              mainAxisSpacing: 10,
-              crossAxisSpacing: 10,
-            ),
-          ),
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  'Tài khoản',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            margin: EdgeInsets.only(top: 10, left: 10, bottom: 10),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10, right: 10),
-            child: Container(
-              color: Colors.grey.shade100,
-              child: const ListTile(
-                leading: Icon(
-                  Icons.logout,
-                  color: Colors.black,
-                ),
-                title: Text(
-                  'Đăng xuất',
-                  textScaleFactor: 1.0,
-                  style: TextStyle(color: Colors.black),
-                ),
-                trailing: Icon(
-                  Icons.arrow_forward,
-                  color: Colors.black,
-                ),
-                selected: true,
+                ],
+                padding: EdgeInsets.all(10),
+                shrinkWrap: true,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
               ),
             ),
-          ),
-        ],
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Tài khoản',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              margin: EdgeInsets.only(top: 10, left: 10, bottom: 10),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Container(
+                color: Colors.grey.shade100,
+                child: const ListTile(
+                  leading: Icon(
+                    Icons.logout,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    'Đăng xuất',
+                    textScaleFactor: 1.0,
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  trailing: Icon(
+                    Icons.arrow_forward,
+                    color: Colors.black,
+                  ),
+                  selected: true,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
